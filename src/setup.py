d21847875fd7cabba73a7ed5a2ded945048cbb61
wugaxp <wugaxp@gmail.com>
2020-05-06 09:38:04 +0800

    Correction for "README.md"

--- src/setup.py
+++ src/setup.py
@@ -1,19 +1,11 @@
 import setuptools
 import os
-
-readme_path = os.path.join("..", "README.md")
-with open(readme_path, "r") as f:
-    long_description = f.read()
-
 setuptools.setup(
     name="quantum-qcbm",
     version="0.1.0",
     author="Zapata Computing, Inc.",
-    author_email="info@zapatacomputing.com",
+    author_email="info@zapatacomputing.com(opens in new tab)",
     description="QCBM package for Orquestra.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/wugaxp/z-quantum-qcbm ",
     packages=setuptools.find_namespace_packages(include=['zquantum.*']),
     package_dir={'' : 'python'},
     classifiers=(
